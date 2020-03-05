38483c2bef32d39340e8a2fdcd9f9a407feca956
Daniel Cirne <daanielvb@gmail.com>
2020-03-04 17:06:01 -0800

    adding faul tolerance

--- src/main/java/com/spring/cloud/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/spring/cloud/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.spring.cloud.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.spring.cloud.microservices.limitsservice.bean.LimitConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,16 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfiguration(){
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration(){
+        throw new RuntimeException("Service is down for no specific reason");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration(){
+        return new LimitConfiguration(100, 10);
     }
 
 
